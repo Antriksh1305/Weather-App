ff03ac94535a4326b63b563a74d313e9c2a9aa7b
Antriksh Gupta <Antrikshgupta01@gmail.com>
2023-06-07 21:51:12 +0530

    App infinite loop response Bugs fixed....

--- App.js
+++ App.js
@@ -16,28 +16,30 @@ export default function App() {
                 console.log("Please grant location permissions");
                 return;
             }
-
-            console.log('reach');
-            const currentLocation = await Location.getCurrentPositionAsync({});
+            else {
+                let s = await Location.requestBackgroundPermissionsAsync();
+            }
             console.log(status);
-            setLocation(currentLocation);
-            console.log("Location:");
-            console.log(currentLocation);
+            function getCurrentLocation() {
+                const timeout = 10000;
+                return new Promise(async (resolve, reject) => {
+                    setTimeout(() => { reject(new Error(`Error getting gps location after ${(timeout * 2) / 1000} s`)) }, timeout * 2);
+                    setTimeout(async () => { resolve(await Location.getLastKnownPositionAsync()) }, timeout);
+                    resolve(await Location.getCurrentPositionAsync());
+                });
+            }
+            const currentLocation =  await getCurrentLocation();
+                setLocation(currentLocation);
+                console.log("Location:");
+                console.log(currentLocation);
         };
-        while (location == undefined) {
-            var timer = setTimeout(() => {
-                if (location == undefined) {
-                    console.log('TIMER');
-                    getPermissions();
-                }
-            }, 2000);
-            return () => clearInterval(timer);
-        }
+        getPermissions();
     }, []);
 
     if (location == undefined) console.log('no location');
     if (location != undefined) return <Index location={location} />;
     else {
+        console.log('here 1');
         return (
             <SafeAreaView style={styles.loader}>
                 <Lottie source={require('./assets/animation/animation.json')} autoPlay loop />
